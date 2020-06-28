21dbfcf001dd34ea408d5c25b06af11426e7f0aa
mikeblazanin <mikeblazanin@gmail.com>
2020-06-28 16:08:29 -0400

    Finished mumford & friman cleanup & export

--- Cleanup_data.R
+++ Cleanup_data.R
@@ -1,4 +1,5 @@
 library(tidyr)
+library(dplyr)
 
 ##Read in Mumford data ----
 
@@ -21,11 +22,40 @@ mum_phg$PhagePresence <- 1
 #Rename Pseudomonas column in mum_bact
 mum_bact$Pseudomonas <- gsub("^PAO$", "PAO1", mum_bact$Pseudomonas)
 
+#Convert timepoints in mum_phg to actual days
+mum_phg$Time_day <- mum_phg$Time*4
 
-#Shared columns: Time, Pseudomonas, Competitor.presence
+#Add Time_day to mum_bact (only measured bact on last day)
+mum_bact$Time_day <- 16
+
+#Join dataframes together
+mum_data <- full_join(mum_phg, mum_bact,
+                      by = c("Pseudomonas", "Competitor.Community",
+                             "PhagePresence", "Time_day"))
+
+#Pivot densities into one column
+mum_data <- pivot_longer(mum_data,
+                         cols = c("PhagesPerML", "PseudomonasCFUperml"),
+                         names_to = "Pop",
+                         values_to = "Density")
+
+##Tidy up
+
+#Shared columns: Time_day, Pseudomonas, Competitor.Community,
+#                 PhagePresence, Pop, Density
 #Uniq cols: Competitor community (for phg includes only non-PA, for bact includes all)
 #           PhagePresence (absent from phage dataframe)
 #           QS (absent from phage dataframe) - but just a shorthand for PAO vs lasR
 #           Treatmentcode - absent from bact, 
 #                           just shorthand for Pseudomonas - Competitor combinations
-#           
+mum_data <- select(mum_data, Pseudomonas, Competitor.Community,
+                   PhagePresence, Time_day, Pop, Density)
+mum_data$Pop[mum_data$Pop == "PhagesPerML"] <- "PT7"
+mum_data$Pop[mum_data$Pop == "PseudomonasCFUperml"] <- "P_aeruginosa"
+mum_data$Competitor.Community[mum_data$Competitor.Community == 0] <- "None"
+mum_data <- mum_data[complete.cases(mum_data),]
+
+#Output clean data
+write.csv(mum_data,
+          "./Clean_data/mumford_friman_data.csv",
+          row.names = FALSE)
