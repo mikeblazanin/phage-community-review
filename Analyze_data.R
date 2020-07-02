a743f0ccae496ca6c599114349935d0ba021723e
mikeblazanin <mikeblazanin@gmail.com>
2020-07-02 12:48:03 -0400

    Fixed up plotting of Alseth & Mumford data

--- Analyze_data.R
+++ Analyze_data.R
@@ -9,6 +9,25 @@ my_cols <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
              "#D55E00", "#CC79A7", "#000000")
 scales::show_col(my_cols)
 
+#Drop NA Density values
+all_data <- all_data[!is.na(all_data$Density),]
+
+#Recode phage presence
+all_data$PhagePresence[all_data$PhagePresence == 1] <- "Phage present"
+all_data$PhagePresence[all_data$PhagePresence == 0] <- "Phage absent"
+
+#Reorder Pop levels
+all_data$Pop <- factor(all_data$Pop,
+                       levels = c("P_aeruginosa", "PT7",
+                                  "PA14", "AB", "BC", "SA", "DMS3vir"))
+
+#Reorder competitor community levels
+all_data$Competitor_community <- 
+  factor(all_data$Competitor_community,
+         levels = c("None", "Sa", "Sm", "Sa+Sm",
+                    "AB", "BC", "SA", "AB+BC+SA", "surfacemutant"))
+                                        
+
 #Get summarized values
 all_data <- group_by(all_data, Study, Focal_strain, Competitor_community,
                      PhagePresence, Time_day, Rep_pop, Pop)
@@ -25,23 +44,31 @@ all_data_repsum <- group_by(all_data_repsum,
 all_data_popsum <- summarize(all_data_repsum,
                              dens_pop_avg = mean(dens_rep_avg),
                              dens_pop_sd = sd(dens_rep_avg, na.rm = TRUE),
-                             dens_pop_n = sum(is.numeric(dens_rep_avg)))
+                             dens_pop_n = sum(sapply(dens_rep_avg, is.numeric)))
 
 
 #Plots ----
-ggplot(data = all_data_repsum[all_data_repsum$Study == "Mumford_Friman", ], 
-       aes(x = Time_day, y = dens_rep_avg+1,
-                            color = Competitor_community)) +
-  geom_point(alpha = 0.5, 
-             position = position_dodge(width = 1.5)) +
-  facet_grid(Pop ~ PhagePresence, scales = "free_y") +
+temp_repsum <- all_data_repsum[all_data_repsum$Study == "Mumford_Friman", ]
+temp_popsum <- all_data_popsum[all_data_popsum$Study == "Mumford_Friman", ]
+ggplot(data = temp_repsum, 
+       aes(x = Time_day, y = dens_rep_avg+1, color = Competitor_community)) +
+  geom_point(alpha = 0.2, position = position_dodge(width = 1.5)) +
+  # geom_line(aes(group = interaction(Rep_pop, Competitor_community)),
+  #           alpha = 0.2, position = position_dodge(width = 1.5)) +
+  geom_point(data = temp_popsum, aes(y = dens_pop_avg+1)) +
+  geom_line(data = temp_popsum, aes(y = dens_pop_avg+1)) +
+  facet_grid(Focal_strain + Pop ~ PhagePresence, scales = "free_y") +
   scale_y_continuous(trans = "log10") +
   scale_color_manual(values = my_cols)
 
-ggplot(data = all_data[all_data$Study == "Alseth_etal", ],
-       aes(x = Time_day, y = Density,
+temp_repsum <- all_data_repsum[all_data_repsum$Study == "Alseth_etal", ]
+temp_popsum <- all_data_popsum[all_data_popsum$Study == "Alseth_etal", ]
+ggplot(data = temp_repsum,
+       aes(x = Time_day, y = dens_rep_avg+1,
            color = Competitor_community)) +
-  geom_point() +
+  geom_point(alpha = 0.2) +
+  geom_point(data = temp_popsum, aes(y = dens_pop_avg+1)) +
+  geom_line(data = temp_popsum, aes(y = dens_pop_avg+1), lwd = 1) +
   facet_grid(Pop~PhagePresence, scales = "free") +
   scale_y_continuous(trans = "log10") +
   scale_color_manual(values = my_cols)
